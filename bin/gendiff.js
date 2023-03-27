ccdf5e8ca338c58b50270354afc4eb380edbdd67
azamat <ahavov_azamat@mail.ru>
2023-03-27 13:57:26 +0300

    second step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,5 +6,7 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .version('-V, --version        output the version number')
-
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
 program.parse();
